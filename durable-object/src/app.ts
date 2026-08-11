b324a94cf22d8fcebc136142f1672740960334f3
machingclee <machingclee@gmail.com>
2026-08-12 07:09:51 +0800

    test sqlite

--- durable-object/src/app.ts
+++ durable-object/src/app.ts
@@ -2,10 +2,19 @@ import { Hono } from 'hono';
 
 const app = new Hono<{ Bindings: Env }>()
 
-app.get('/hello', (c) => {
+app.get("/register/:username", async (c) => {
+    const username = c.req.param("username");
     const stub = c.env.MY_DURABLE_OBJECT.getByName("foo");
-    stub.sayHello("world");
-    return c.json({ message: 'hi' })
+    const result = await stub.createUser(username);
+    return c.json({ result: { id: result } })
 })
+
+app.get("/user/:id", async (c) => {
+    const id = c.req.param("id");
+    const stub = c.env.MY_DURABLE_OBJECT.getByName("foo");
+    const result = await stub.getUser(parseInt(id));
+    return c.json({ result: { user: result } })
+})
+
 
 export default app;
--- durable-object/src/durable-object.ts
+++ durable-object/src/durable-object.ts
@@ -2,21 +2,34 @@ import { DurableObject } from "cloudflare:workers";
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
 export class MyDurableObject extends DurableObject<Env> {
+    private sql: SqlStorage
+
     constructor(ctx: DurableObjectState, env: Env) {
         super(ctx, env);
-    }
-    async sayHello(name: string): Promise<string> {
-        return `Hello, ${name}!`;
+
+        this.sql = this.ctx.storage.sql;
+        ctx.blockConcurrencyWhile(async () => {
+            this.sql.exec(`
+        CREATE TABLE IF NOT EXISTS users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT NOT NULL UNIQUE
+        )`);
+        });
     }
 
-    async createAlarm() {
-        const alarm = await this.ctx.storage.getAlarm();
-        if (!alarm) {
-            this.ctx.storage.setAlarm(Date.now() + 1000 * 60 * 5); // Set an alarm for 5 minutes from now
-        }
+    async createUser(username: string): Promise<number> {
+        const result = this.sql.exec<{ id: number }>(
+            "INSERT INTO users (username) VALUES (?) RETURNING id",
+            username
+        );
+        return result.one().id;
     }
 
-    async alarm(_alarmInfo?: AlarmInvocationInfo): Promise<void> {
-        // no-op
+    async getUser(id: number): Promise<{ id: number; username: string } | null> {
+        const result = this.sql.exec<{ id: number; username: string }>(
+            "SELECT id, username FROM users WHERE id = ?",
+            id
+        );
+        return result.toArray()[0] ?? null;
     }
 }
